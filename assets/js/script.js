97ce8ab1931d076f6cc704e537879025825e7aed
J Diaz <j.e.diaz@icloud.com>
2021-05-16 23:23:03 -0700

    somewhat works again

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,59 +13,9 @@ var ApiKey = "c511e5d07f5ce85a7063eeb997c17013";
 
 var savedCities = [];
 
-// $("#searchBtn").click(function () {
-//     var searchTerm = $("#search").val();
-//       fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${ApiKey}&units=imperial`) 
-//         .then(function (data) {
-//           return data.json();
-//         })
-//         .then(function (data) {
-//           console.log(data);
-//           // console.log(data.name); //the name of the city
-//           // console.log(data.main.temp);//the current temp
-//           // console.log(data.main.humidity);//the humidity
-//           // console.log(data.wind.speed)//the wind speed
-//           // console.log(data.dt)// this should be the unix time stamp
-//           var citiesNames = data.name;
-//           var currentTemp = data.main.temp;
-//           var currentHumidity =data.main.humidity;
-//           var currentWind = data.wind.speed;
-//           var dataLat = data.coord.lat;//need this for uv index data pull
-//           var dataLon = data.coord.lon;// need this for uv index data pull
-//           var unixtime =data.dt// var needed to convert to
-//           var dateInf = moment.unix(unixtime).format("MM/DD/YYYY");
-//           $("#saved-cities").append(`
-//           <button type="button" class="btn btn-outline-dark col-12" id="${citiesNames}">${citiesNames}</button>
-//           `);
-//           $("#weather-info").append(`
-//           <div class="card col-12" >
-//           <div class="card-body">
-//             <h1 class="card-title" data-cityName=>${citiesNames} ${dateInf}</h1>
-//             <h2 class="card-subtitle mb-2 text-muted">Temperature: ${currentTemp} °F</h2>
-//             <h2 class="card-subtitle mb-2 text-muted">Humidity: ${currentHumidity} %</h2>
-//             <h2 class="card-subtitle mb-2 text-muted">Wind Speed: ${currentWind} MPH</h2>
-//           </div>
-//         </div>
-//           `)
-//           //nested fetch to pull the uv index data
-//           fetch(`http://api.openweathermap.org/data/2.5/uvi?lat=${dataLat}&lon=${dataLon}&appid=${ApiKey}`)
-//           .then(function (data) {
-//             return data.json();
-//           })
-//           .then(function(data){
-//             // console.log(data);
-//             // console.log(data.value)// this is the uv index
-//             var uvIndex = parseFloat(data.value);
-//             console.log(uvIndex)
-//             $("#weather-info").append(`
-//             <h2 class="card-subtitle mb-2 text-muted">UV Index: <h2 class="card-subtitle mb-2" style="background-color: red; color: black; width: fit-content; "> ${uvIndex}</h2></h2>
-//             `)
-//           });
-//         });
-//         // This should make it to were the function starts over if no name is entered
-//         if($("#search").val("")){
-//         }return;
-//     });
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+// partial working code below 
+// do not delete!!!
 
 function dayInfoSection(){
   $("#searchBtn").click(function () {
@@ -115,7 +65,7 @@ function dayInfoSection(){
             $("#weather-info").append(`
             <h2 class="card-subtitle mb-2 text-muted">UV Index: <h2 class="card-subtitle mb-2" style="background-color: red; color: black; width: fit-content; "> ${uvIndex}</h2></h2>
             `)
-                    //this will add the city to the local storage array of cities
+         //this will add the city to the local storage array of cities
         savedCities.push({
           "city":citiesNames,
           "temp":currentTemp,
@@ -134,13 +84,12 @@ function dayInfoSection(){
 }
 dayInfoSection();
 
+console.log(savedCities);
+
 // will add the week info
 // function weekInfoSection(){
 //   $("#searchBtn").click(function () {
-// for (var i = 0; i< savedCities.length; i++){
-//   console.log(savedCities);
-//   console.log(savedCities[i].city);
-//   fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${savedCities[i].city}&appid=${ApiKey}`) 
+//   fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&appid=${ApiKey}`) 
 //   .then(function (data) {
 //     return data.json();
 //   })
@@ -148,9 +97,7 @@ dayInfoSection();
 //     console.log(data);
 //   })
 // }
-
-//   });
-// }
+//   )};
 // weekInfoSection()
 
 
